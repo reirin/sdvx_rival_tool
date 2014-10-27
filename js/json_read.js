9fc86a0426680f58231e4ca236be8d0a1be09dd3
reirin <reirin_nyanko_88@yahoo.co.jp>
2014-10-27 13:56:06 +0900

    少し整理した

--- js/json_read.js
+++ js/json_read.js
@@ -1,32 +1,43 @@
+
+var mydatafile = "http://sdvx-s.coresv.com/user/reirin.json?callback=?";
+var rivaldatafile = "http://sdvx-s.coresv.com/user/tunedoc.json?callback=?";
+var rivaldatafile2 = "http://sdvx-s.coresv.com/user/sample.json?callback=?";
+var testdatafile = "testjson.json";
+
 $(function () {
 	$("#start").click(function(){
-		console.log("スタートがおされたよ");
-		makeList();
+		// makeList();
 	});
 
-	$("#listset").click(function(){
-		console.log("押されたよ");
-	});
+	$("#listset").click(makeList);
 });
 
 function makeList() {
-		//ネットからJsonファイルを取得する場合こっち
-		var mydatafile = "http://sdvx-s.coresv.com/user/reirin.json?callback=?";
-		var testdatafile = "testjson.json";
 
 		$.getJSON(mydatafile,function(data){
-			myfile = data;
-			console.log(myfile);
+			console.log(data);
 			var len = data.profile.tracks.length,
 				// dia = "dialog",
 				//" data-rel = "+dia+
 				$tid = $("#tracks");
 
 				for (var i = 0; i < len; i++) {
+					var nsc = data.profile.tracks[i].novice.highscore;
+					var asc = data.profile.tracks[i].advanced.highscore;
+					var esc = data.profile.tracks[i].exhaust.highscore;
+					if (nsc == undefined){
+						nsc = 0;
+					}
+					if (asc == undefined){
+						asc = 0;
+					}
+					if (esc == undefined){
+						esc = 0;
+					}
 					$tid.prepend(
-						"<li id = "+ i +"><a href = \"#score\" data-rel = \"popup\" data-transition = \"pop\" class = \"ui-btn ui-corner-all ui-btn-icon -left ui-icon-check\">" + data.profile.tracks[i].title + "</a></li>");
+						"<li id = "+ i +"><a href = \"#score\" data-rel = \"popup\" data-transition = \"pop\" class = \"ui-btn ui-corner-all ui-btn-icon -left ui-icon-check\">" + data.profile.tracks[i].title + "<br> exhaust : "+esc+"</a></li>");
 				}
-				$tid.listview("refresh");
+				// $tid.listview("refresh");
 				scoreDisp(data);
 			});
 	}
@@ -70,7 +81,7 @@ function makeList() {
 		if(data.profile.tracks[tracksid].infinite != undefined){
 			if(data.profile.tracks[tracksid].infinite.highscore != undefined){
 				ps = data.profile.tracks[tracksid].infinite.highscore;
-				pg = data.profile.tracks[tracksid].inginite.grade;
+				pg = data.profile.tracks[tracksid].infinite.grade;
 			}else{
 				ps = 0;
 				pg = "no play";
